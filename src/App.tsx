317500edd8d94f9ae1be7908b238bae6fc4c9342
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-04-20 18:09:18 +0800

    create useState for add CourseGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import CourseGoal from "./components/CourseGoal";
 import goalImage from "./assets/goals.jpg";
+import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
+import { useState } from "react";
 
+interface CourseGoal {
+  title: string;
+  description: string;
+  id: number;
+}
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoals: CourseGoal = {
+        id: Math.random(),
+        title: "tajuk robin",
+        description: "robin terlalu kacak",
+      };
+      return [...prevGoals, newGoals];
+    });
+  }
   return (
     <div>
       <main>
         <Header image={{ src: goalImage, alt: "List of goals" }}>
           <h1>List of goal</h1>
         </Header>
-        <CourseGoal title="Tajuk">
-          <p>children paragraph</p>
-        </CourseGoal>
+        <button onClick={handleAddGoal}>Add Goal</button>
+        <ul>
+          {goals.map((goal) => (
+            <li key={goal.id}>
+              <CourseGoal title={goal.title}>
+                <p>{goal.description}</p>
+              </CourseGoal>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   );
